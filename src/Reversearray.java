cfd5580b469d431ad921d25c9597a8464dd116b2
Sushantt771 <sushant.gupta771@gmail.com>
2024-04-20 19:03:09 +0530

    intial commit

--- src/Reversearray.java
+++ src/Reversearray.java
@@ -6,6 +6,7 @@ public class Reversearray {
             for (int i = 0; i < arr.length; i++) {
                 System.out.print(arr[i] + " ");
             }
+
             System.out.println();
             System.out.println("Array in reverse order: ");
             //Loop through the array in reverse order
