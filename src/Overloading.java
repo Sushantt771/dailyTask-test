90f6f8dbf7b0e593c656648d5adf14a028e081a3
Sushantt771 <163402987+Sushantt771@users.noreply.github.com>
2024-04-25 22:55:39 +0530

    Update Overloading.java

--- src/Overloading.java
+++ src/Overloading.java
@@ -1,14 +1,34 @@
-public class Overloading {
+public class Calculator {
 
-    void demo (int a) {
-        System.out.println ("a: " + a);
+    // Method to add two integers
+    public int add(int a, int b) {
+        return a + b;
     }
-    void demo (int a, int b) {
-        System.out.println ("a and b: " + a + "," + b);
+
+    // Overloaded method to add three integers
+    public int add(int a, int b, int c) {
+        return a + b + c;
     }
-    double demo(double a) {
-        System.out.println("a: " + a);
-        return a*a;
+
+    // Overloaded method to add two doubles
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        // Calling the add method with two integers
+        int sum1 = calculator.add(5, 10);
+        System.out.println("Sum of 5 and 10: " + sum1);
+
+        // Calling the add method with three integers
+        int sum2 = calculator.add(5, 10, 15);
+        System.out.println("Sum of 5, 10, and 15: " + sum2);
+
+        // Calling the add method with two doubles
+        double sum3 = calculator.add(2.5, 3.5);
+        System.out.println("Sum of 2.5 and 3.5: " + sum3);
     }
 }
 
